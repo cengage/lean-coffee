b8e8e5f4468ea29cb003696eb396203abdd7d2d0
harsha <harsha@harsha-Lenovo-U410.(none)>
2014-08-07 11:02:33 -0400

    separate production database instance

--- public/js/database.js
+++ public/js/database.js
@@ -3,7 +3,11 @@
  */
 module.exports = {
 
-    // the database url to connect
-    url : 'mongodb://root:root@novus.modulusmongo.net:27017/Hew4ojum'
+    // the database url to connect - development
+//    url : 'mongodb://root:root@novus.modulusmongo.net:27017/Hew4ojum'
+
+    // the database url to connect - production
+    url : 'mongodb://root:root@novus.modulusmongo.net:27017/yvEsip9i'
+
 //    url: 'mongodb://localhost:27017/test'
 }
