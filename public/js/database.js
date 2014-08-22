30d6a59170d21113d902f3b8c1fe37b8cafe9557
Harsha <harsh.ssd@gmail.com>
2014-08-22 13:43:17 -0400

    migration of DB to Lean Coffee account

--- public/js/database.js
+++ public/js/database.js
@@ -4,10 +4,10 @@
 module.exports = {
 
     // the database url to connect - for test and development
-//    url : 'mongodb://root:root@proximus.modulusmongo.net:27017/bunaG6uv'
+//    url : 'mongodb://root:root@proximus.modulusmongo.net:27017/qib2aqaX'
 
     // the database url to connect - for production
-    url : 'mongodb://root:root@novus.modulusmongo.net:27017/yvEsip9i'
+    url : 'mongodb://root:root@proximus.modulusmongo.net:27017/bunaG6uv'
 
 //    url: 'mongodb://localhost:27017/test'
 }
